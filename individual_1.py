7a1660e8de4a8a856effd4a2911fb1169bc2a4df
Marat <125448969+martini69lol@users.noreply.github.com>
2023-05-10 13:57:30 +0300

    Update individual_1.py

--- individual_1.py
+++ individual_1.py
@@ -1,6 +1,6 @@
-def star():
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
     word = input('Введите слово: ')
     print('*'*len(word)+word+'*'*len(word))
-
-    
-star()
